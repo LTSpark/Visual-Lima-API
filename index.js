bd0f338a21dc07f994880b62af13683cea98189a
Leonardo Torres <dleonardotb2001@gmail.com>
2023-03-05 20:59:20 -0500

    iot project v2: endpoints

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const express = require("express");
 // Initialize Express
 const app = express();
 
+app.use(cors);
+
+app.post("/aws-core", (req, res) => {
+  res.send(req.body)
+});
+
+app.post("/aws-core/data", (req, res) => {
+  res.send(req.body)
+});
+
 // Create GET request
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
